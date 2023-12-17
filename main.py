c97783a41c11a770d3f5e8d71ad640700c0370b0
oblvisheee <molniya213y@gmail.com>
2023-12-18 00:14:18 +0300

    Added error system.

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ def generate(message):
     prompt = message.text
     bot.send_chat_action(message.chat.id, 'typing')
     bot.send_message(message.chat.id, 'Generating your image...')
-    image_url = generate_image(prompt)
-    image_path = os.path.join(os.getcwd(), 'image.jpg')
-    save_image(image_url, image_path)
-    bot.send_chat_action(message.chat.id, 'upload_photo')
-    with open(image_path, 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-    os.remove(image_path)
-    bot.send_message(message.chat.id, 'Your image has been generated!')
+    try:
+        image_url = generate_image(prompt)
+        image_path = os.path.join(os.getcwd(), 'image.jpg')
+        save_image(image_url, image_path)
+        bot.send_chat_action(message.chat.id, 'upload_photo')
+        with open(image_path, 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+        os.remove(image_path)
+        bot.send_message(message.chat.id, 'Your image has been generated!')
+    except:
+        bot.send_message(message.chat.id, 'Something went wrong, possibly, that was an NSFW alert, or problem with API, try again please.')
 
 bot.polling(non_stop=True)
 
